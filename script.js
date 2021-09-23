269bf4d6f48c8b6b5033662e9ac90fff99c178df
Maurice Collier <mauricecollier84@yahoo.com>
2021-09-23 11:43:17 -0400

    Added the Bounus plus other versions.

--- script.js
+++ script.js
@@ -42,7 +42,9 @@ console.log(randomNumber);
 
 // Bonus
 // 6
-
+console.log(" Let It Go!".toUpperCase().repeat(2).trim);
+// You can add multiple arguments in the argument sections aka parenthesis. (Chaining methods together)
+// No order is necessary in these multiple arguments.
 
 // 7a
 let reindeers = "Reindeers are better than people.";
@@ -50,16 +52,40 @@ let reindeers = "Reindeers are better than people.";
 // 7b
 reindeers = ("Reindeers_are_better_than_people.");
 console.log(reindeers);
+// console.log(reindeers.replace(/ /g, "_"));
+// g = global. Means select all the instances of space inside the / /.
 
 // 8
 console.log(Math.sqrt(2));
+// console.log(Math.SQRT2);
 
 // 9
-let newrandomNumber = Math.random();
-newrandomNumber *= 23;
-newrandomNumber = Math.ceil(newrandomNumber);
-newrandomNumber = Math.floor(newrandomNumber);
-console.log(newrandomNumber);
+// let newrandomNumber = Math.random();
+// newrandomNumber *= 23;
+// newrandomNumber = Math.floor(newrandomNumber);
+// console.log(newrandomNumber);
+var newrandomNumber = Math.floor(Math.random() * (23 - 7 + 1) + 7);
+// Order of operations method.
+// var newrandomNumber = Math.floor(Math.random() * 17 + 7); (17 sets the range and 7 sets the floor)
+
+// 9
+// Completed on separate lines
+// let newRandomNumber = Math.random();
+// newRandomNumber *= 17;
+// newRandomNumber = Math.floor(newRandomNumber);
+// newRandomNumber += 7;
+
+// Extra Bonus: Completed on a single line
+var newRandomNumber = Math.floor(Math.random() * (23 - 7 + 1) + 7);
+// var newRandomNumber = Math.floor(Math.random() * (16 + 1) + 7);
+// var newRandomNumber = Math.floor(Math.random() * 17 + 7);
+// var newRandomNumber = Math.floor(A number from 0 up to 16.999 + 7);
+// var newRandomNumber = Math.floor(A number from 7 up to 23.999);
+// var newRandomNumber = A whole number from 7 up to 23;
+console.log(newRandomNumber);
+
+// Using Ceil (Number could possibly be 6 in this case)
+let randomNumber = Math.ceil(Math.random() * 17) + 6;
 
 
 
