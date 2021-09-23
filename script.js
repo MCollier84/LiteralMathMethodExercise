398721531076ea1919285da3e2a819314ce53571
Maurice Collier <mauricecollier84@yahoo.com>
2021-09-23 10:57:10 -0400

    Added other ways to complete the assignment.

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
 
 // 1b
 console.log(warmHugs.toUpperCase());
+// You can insert the variable name or value in the argument aka parenthesis.
 
 // 1c
-console.log(warmHugs.replace("like", "love"))
+console.log(warmHugs.replace("like", "love"));
+// warmHugs = warmHugs.replace("like", "love")
 // let warmHugs = "Hi, I'm Olaf and I love warm hugs.";
 
 // 2a
@@ -13,12 +15,15 @@ let beenImpaled = "Oh, look at that. I've been impaled.";
 
 // 2b
 console.log(beenImpaled.slice(18));
+// console.log(beenImpaled.slice(18, 36)); (The second number should be the number after what you want to include. 36 instead of 35)
+// console.log(beenImpaled.slice(-18));
 
 // 3a
 let dotDotDot = "...";
 
 // 3b
 let skullBones = "I don't have a skull" + dotDotDot + "or bones.";
+// let skullBones = `I don't have a skull${skullBones}or bones.`;
 
 // 4
 console.log(Math.PI);
@@ -26,9 +31,14 @@ console.log(Math.PI);
 // 5
 let randomNumber = Math.random();
 randomNumber *= 3;
-randomNumber = Math.ceil(randomNumber);
+randomNumber = Math.ceil(randomNumber); // Try not to use .ceil when creating a random number.
 randomNumber = Math.floor(randomNumber);
 console.log(randomNumber);
+// let randomNumber = Math.random();
+// randomNumber *= 3;
+// randomNumber = Math.floor(randomNumber);
+// randomNumber++;
+// console.log(randomNumber);
 
 // Bonus
 // 6
