a06dc4daddca6befc67265dcc8ea0aceab2f94ee
Maurice Collier <mauricecollier84@yahoo.com>
2021-09-22 20:29:29 -0400

    Created the LiteralMathMethodExercise file.

---
+++ script.js
@@ -0,0 +1,37 @@
+// 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+
+// 1b
+console.log(warmHugs.toUpperCase());
+
+// 1c
+console.log(warmHugs.replace("like", "love"))
+// let warmHugs = "Hi, I'm Olaf and I love warm hugs.";
+
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled.";
+
+// 2b
+console.log(beenImpaled.slice(18));
+
+// 3a
+let dotDotDot = "...";
+
+// 3b
+let skullBones = "I don't have a skull" + dotDotDot + "or bones.";
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.random();
+randomNumber *= 3;
+randomNumber = Math.ceil(randomNumber);
+randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
+
+
+
+
+
+
